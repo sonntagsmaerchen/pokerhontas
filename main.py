7f35ac2a980252779728b2681db7ace49701cecd
yuki <yuki77x@googlemail.com>
2016-12-29 14:07:42 +0100

    Remove while loop

--- main.py
+++ main.py
@@ -11,10 +11,6 @@ fileTime = os.path.getmtime(filePath)
 
 fileInput = open(filePath, "r", 1)
 
-done = False
-while not done:
-    data = follow(fileInput)
-    for line in data:
-        print(line)
-
-    break
+data = follow(fileInput)
+for line in data:
+    print(line)
